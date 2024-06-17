4e020e62224bcafd3cddca5a5066c02c1f8b9298
Latanks <76797064+LAtanks@users.noreply.github.com>
2024-06-17 18:00:35 -0300

    First commit

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.2.6"
+	id("io.spring.dependency-management") version "1.1.5"
+}
+
+group = "br.com.latanks"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	compileOnly("org.projectlombok:lombok:1.18.32")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
