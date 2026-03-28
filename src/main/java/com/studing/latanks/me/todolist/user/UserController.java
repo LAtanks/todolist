9c984b9aea21dcb3965d2d634184e676e054583e
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-03-28 18:59:07 -0300

    Create User controller & model and add Lombok Dependency in project

---
+++ src/main/java/com/studing/latanks/me/todolist/user/UserController.java
@@ -0,0 +1,18 @@
+package com.studing.latanks.me.todolist.user;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    @PostMapping("/")
+    public void create(@RequestBody UserModel user) {
+        System.out.println("Creating user: " + user.getUsername() + ", " + user.getName());
+
+    }
+
+}
---
+++ src/main/java/com/studing/latanks/me/todolist/user/UserModel.java
@@ -0,0 +1,10 @@
+package com.studing.latanks.me.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    private String username;
+    private String name;
+    private String password; 
+}
