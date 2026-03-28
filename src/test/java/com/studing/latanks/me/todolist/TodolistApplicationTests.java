cf90c95be036e9287b47aa57c67e26719f5c90a0
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-03-28 18:18:28 -0300

    Create Spring Boot Project TodoList

---
+++ src/test/java/com/studing/latanks/me/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studing.latanks.me.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
