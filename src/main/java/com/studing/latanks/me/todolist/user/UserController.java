7b08875b80fbf37ac7a78d6fc05a607577576001
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-03-28 21:22:00 -0300

    Add Hash Password for user

--- src/main/java/com/studing/latanks/me/todolist/user/UserController.java
+++ src/main/java/com/studing/latanks/me/todolist/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -22,6 +24,9 @@ public class UserController {
             System.out.println("User already exists with username: " + user.getUsername());
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User already exists"); // Or throw an exception, or return a specific response indicating the user already
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
+        user.setPassword(passwordHashred);
         var userCreated = this.userRepository.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
