6f39ec3d6145fb2c95438d6d21ed667f5aa6f049
Giovanni Samuel <76797064+LAtanks@users.noreply.github.com>
2026-03-30 22:30:12 -0300

    Add tratamento de error na aplicação

--- src/main/java/com/studing/latanks/me/todolist/task/TaskModel.java
+++ src/main/java/com/studing/latanks/me/todolist/task/TaskModel.java
@@ -11,6 +11,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
 
+
 @Data
 @Entity(name = "tb_tasks")
 public class TaskModel {
@@ -31,4 +32,11 @@ public class TaskModel {
 
     
     private UUID userId;
+
+    public void setTitle(String title) throws Exception {
+        if(title.length() > 50){
+            throw new Exception("Title must be less than 50 characters");
+        }
+        this.title = title;
+    }
 }
--- src/main/java/com/studing/latanks/me/todolist/user/UserController.java
+++ src/main/java/com/studing/latanks/me/todolist/user/UserController.java
@@ -28,7 +28,7 @@ public class UserController {
         var passwordHashred = BCrypt.withDefaults().hashToString(12, user.getPassword().toCharArray());
         user.setPassword(passwordHashred);
         var userCreated = this.userRepository.save(user);
-        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+        return ResponseEntity.status(HttpStatus.OK).body(userCreated);
     }
 
 }
